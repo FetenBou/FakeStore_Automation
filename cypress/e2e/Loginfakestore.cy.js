d668d3a97e4a6b8745ef6051768d9ccd781c5b63
Feten Boujmil <AL1097268@admin.hubtotal.net>
2026-06-07 13:07:43 +0200

    use fixtures to pass

--- cypress/e2e/Loginfakestore.cy.js
+++ cypress/e2e/Loginfakestore.cy.js
@@ -1,16 +1,22 @@
 import { AUTH } from "../constants/endpoints";
 
-describe("login feature " ,() => {
 
+describe("login feature " ,() => {
+  let loginData;
+  // extract user data from fixutres 
+ beforeEach(() => {
+
+    cy.fixture("login_data").then((data) => {
+      loginData = data;
+    });
+
+  });
 //postitve case : 
  it("login request with passing credentials ", () => {
   cy.request({
   method: 'POST',
   url:  AUTH.auth,
-  body: {
-    username: 'mor_2314',
-    password: '83r5^_'
-  }
+  body: loginData.validUser
 }).then((response) => {
   expect(response.status).to.eq(201)
   expect(response.body).to.have.property('token')
@@ -23,10 +29,7 @@ it("login request with false credentials ", () => {
   cy.request({
   method: 'POST',
   url:  AUTH.auth,
-  body: {
-    username: 'xxx_2314',
-    password: '83r5^_'
-  },
+  body:loginData.invalidUser ,
   failOnStatusCode: false,
 }).then((response) => {
   expect(response.status).to.eq(401)
